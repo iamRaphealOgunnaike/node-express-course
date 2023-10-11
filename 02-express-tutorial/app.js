7bd604971e170110569a8adcd347f319d79f4ca5
SCI Africa <scitech4africa@gmail.com>
2023-10-11 14:15:00 -0700

    wednesday commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,29 +1,25 @@
-const http = require("http");
-const { url } = require("inspector");
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url);
-  const url = req.url;
 
-  //home page
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h2>home page</h2>");
-    res.end();
-  }
-  // about page
-  else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h2>about page</h2>");
-    res.end();
-  }
-  //error page 404
-  else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h2>Page not found</h2>");
-    res.end();
-  }
+app.get('/', (req, res)=>{
+  res.status(200).send('Home page')
+})
+app.get('/about', (req, res)=>{
+  res.status(200).send('about page')
+})
 
-});
+app.all('*', (req, res)=>{
+  res.status(404).send('<h1>resource not found</h1>')
+})
 
-server.listen(1984);
+
+app.listen(1984, ()=>{
+  console.log('server is listening on port 1984.....')
+})
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
