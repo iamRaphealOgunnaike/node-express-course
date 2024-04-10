7c02ca4b9b1c151580607519339dcd2554b2f44b
iamRaphealOgunnaike <107220283+iamRaphealOgunnaike@users.noreply.github.com>
2024-04-10 03:34:49 -0700

    Apirl 2024 commit

---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,10 @@
+const Product = require('../models/product')
+const getAllProductsStatic = async(req, res) => {
+    throw new Error('testing async errors')
+    res.status(200).json({msg:'products testing route'})
+}
+const getAllProducts = async(req, res) =>{
+    res.status(200).json({msg:'products route'})
+}
+
+module.exports= { getAllProducts, getAllProductsStatic}
