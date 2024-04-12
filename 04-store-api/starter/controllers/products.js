fb6c9d9d1e64788ec96a38044b31af6320c974fb
iamRaphealOgunnaike <107220283+iamRaphealOgunnaike@users.noreply.github.com>
2024-04-11 20:52:57 -0700

    Second commit in Apirl 2024

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,31 @@
 const Product = require('../models/product')
-const getAllProductsStatic = async(req, res) => {
-    throw new Error('testing async errors')
-    res.status(200).json({msg:'products testing route'})
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({}).select('name price')
+    res.status(200).json({ products, nbHits: products.length })
 }
-const getAllProducts = async(req, res) =>{
-    res.status(200).json({msg:'products route'})
+const getAllProducts = async (req, res) => {
+    const { featured, company, name,sort } = req.query
+    const queryObject = {}
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if (name) {
+        queryObject.name = { $regex: name , $options: 'i' }
+    }
+    //console.log(queryObject)
+    let result =  Product.find(queryObject)
+    if(sort){
+        const sortList = sort.split(',').join('');
+        result = result.sort(sortList)
+    }
+    else{
+        result = result.sort('createAt')
+    }
+    const products = await result
+    res.status(200).json({ products, nbHits: products.length })
 }
 
-module.exports= { getAllProducts, getAllProductsStatic}
+module.exports = { getAllProducts, getAllProductsStatic }
